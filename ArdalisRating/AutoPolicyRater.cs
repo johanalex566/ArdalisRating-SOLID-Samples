be685eb3040da242187e78c86ec18ef7707aa5ca
johanalex566 <johanalex566@gmail.com>
2023-05-08 20:43:00 -0500

    Open close principle

---
+++ ArdalisRating/AutoPolicyRater.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdalisRating
+{
+    public class AutoPolicyRater : Rater
+    {
+        private readonly RatingEngine _engine;
+        private ConsoleLogger _logger;
+
+        public AutoPolicyRater(RatingEngine engin, ConsoleLogger logger): base(logger)
+        {
+            _engine = engin;
+            _logger = logger;
+        }
+
+        public override void Rate(Policy policy)
+        {
+            _logger.Log("Rating AUTO policy...");
+            _logger.Log("Validating policy.");
+
+            if (String.IsNullOrEmpty(policy.Make))
+            {
+                _logger.Log("Auto policy must specify Make");
+                return;
+            }
+            if (policy.Make == "BMW")
+            {
+                if (policy.Deductible < 500)
+                {
+                    _engine.Rating = 1000m;
+                }
+                _engine.Rating = 900m;
+            }
+        }
+
+    }
+}
---
+++ ArdalisRating/Rater.cs
@@ -0,0 +1,14 @@
+﻿namespace ArdalisRating
+{
+    public abstract class Rater
+    {
+        public ConsoleLogger Logger { get; set; }
+
+        public Rater(ConsoleLogger logger)
+        {
+            Logger = logger;
+        }
+
+        public abstract void Rate(Policy policy);
+    }
+}
